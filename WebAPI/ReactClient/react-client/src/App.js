457d79b7a13fd28e1bbe1b0c8d904017e06a9a1b
DDianovMD <ddianovmd@abv.bg>
2023-07-04 20:08:02 +0300

    Refactored ReactClient to use PrimeReact components.

--- WebAPI/ReactClient/react-client/src/App.js
+++ WebAPI/ReactClient/react-client/src/App.js
@@ -1,29 +1,37 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { NavBar } from './Components/NavBar';
-import { Home } from './Components/Home.js'
-import { Edit } from './Components/Edit';
-import { Add } from './Components/Add';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import { NavBar } from './Components/NavBar';
+// import { Home } from './Components/Home.js'
+// import { Edit } from './Components/Edit';
+// import { Add } from './Components/Add';
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+// } from "react-router-dom";
+
+import { PrimeReactProvider } from 'primereact/api';
+import { QueryClientProvider, QueryClient } from 'react-query';
+import { PrimeReactHomePage } from './Components/PrimeReactHomePage';
+
+const queryClient = new QueryClient();
 
 function App() {
-
-  
-
   return (
     <div className="App">
-      <Router>
+      {/* <Router>
         <NavBar />
         <Routes>
           <Route exact path='/' element={<Home />}></Route>
-          <Route path='/edit' element={<Edit />}></Route>
-          <Route exact path='/add' element={<Add />}></Route>
+           <Route path='/edit' element={<Edit />}></Route>
+           <Route exact path='/add' element={<Add />}></Route>
         </Routes>
-      </Router>
+      </Router>  */}
+      <QueryClientProvider client={queryClient}>
+        <PrimeReactProvider>
+          <PrimeReactHomePage></PrimeReactHomePage>
+        </PrimeReactProvider>
+      </QueryClientProvider>
     </div>
   );
 }
